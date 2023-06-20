4aad7b2f5e70c67a4d87c075252f6b47b6be3629
a.chirkin <a.chirkin@robo.finance>
2023-06-20 16:18:53 +0700

    add FileSystemRepositoryTests

---
+++ tests/Unit/FileSystemRepositoryTests/FileSystemRepositoryTests.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Unit\FileSystemRepositoryTests;
+
+use App\Services\Repository\FileSystemRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class FileSystemRepositoryTests extends TestCase
+{
+    private function createFile($fileName = null)
+    {
+        return UploadedFile::fake()->create($fileName ?? Str::random(10) . '.docx');
+    }
+
+    public function test_repository_can_save_file(): void
+    {
+        $fileSystemRepository = new FileSystemRepository();
+        Storage::fake('documents');
+        $fileName = 'document.docx';
+
+        $file = $this->createFile($fileName);
+
+        $fileSystemRepository->saveFile($file);
+
+        Storage::disk('documents')->assertExists($file->getClientOriginalName());
+    }
+
+    public function test_repository_can_check_is_file_exists(): void
+    {
+        $fileSystemRepository = new FileSystemRepository();
+        Storage::fake('documents');
+        $fileExistsName = 'document.docx';
+        $fileNotExistsName = 'test.docx';
+
+        $fileExists = $this->createFile($fileExistsName);
+        $fileNotExists = $this->createFile($fileNotExistsName);
+
+        $fileSystemRepository->saveFile($fileExists);
+
+        $this->assertTrue($fileSystemRepository->fileExists($fileExists->getClientOriginalName()));
+        $this->assertFalse($fileSystemRepository->fileExists($fileNotExists->getClientOriginalName()));
+    }
+
+    public function test_repository_can_get_file_path(): void
+    {
+        $fileSystemRepository = new FileSystemRepository();
+        Storage::fake('documents');
+
+        $file = $this->createFile();
+        $fileSystemRepository->saveFile($file);
+
+
+        $storagePath = Storage::disk('documents')->path($file->getClientOriginalName());
+        $fileSystemRepositoryPath = $fileSystemRepository->getFilePath($file->getClientOriginalName());
+
+        $this->assertEquals($storagePath, $fileSystemRepositoryPath);
+    }
+
+    public function test_repository_can_delete_file(): void
+    {
+        $fileSystemRepository = new FileSystemRepository();
+        Storage::fake('documents');
+
+        $file = $this->createFile();
+        $fileSystemRepository->saveFile($file);
+        Storage::disk('documents')->assertExists($file->getClientOriginalName());
+
+        $fileSystemRepository->deleteFile($file->getClientOriginalName());
+        Storage::disk('documents')->assertMissing($file->getClientOriginalName());
+    }
+}
